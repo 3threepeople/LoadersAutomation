f023eabab0908a2ea082ff0b33e84aaafb302559
shreyas <shreyas.hosur@evivehealth.com>
2019-05-02 18:55:48 +0530

    Adding Round 3 test files

--- src/main/java/Models/Stats.java
+++
@@ -1,9 +0,0 @@
-package Models;
-
-public class Stats {
-  public static final String VALIDJSONS="NEW JSONS";
-  public static final String INVALIDJSONS="INVALID JSONS";
-  public static final String OVERRIDEDJSONS="OVERRIDED JSONS";
-  public static final String TOTALJSONS="TOTAL JSONS";
-  public static final String TOTALTIMETAKEN="Total Time Taken";
-}
